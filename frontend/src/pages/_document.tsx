68d0b35381607d39c6208cc8d381f3a474e4b520
Dee <igbokedan48@gmail.com>
2025-11-18 08:49:12 +0100

    Add Google Search Console verification meta tag

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="CVvQYNmBSqnU6lUOmH8zpaNorUOgH_gjskwM8nN8aMc"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
